738acf2171816e18657aac574ae693d1997ddfd1
giangy08 <98910495+giangy08@users.noreply.github.com>
2022-11-10 09:50:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 from flask import Flask, request, werkzeug
 
 app = Flask(__name__)
-app.secret_key = "ciaone"
+app.secret_key = "my_secrek_key_1"
 
 #selecting a route for our app
-@app.route('/dataexchange',methods = ['POST'])
-def dataexchange():
-   if request.method == 'POST':
-       imagefile = Flask.request.files['image']
-       filename = werkzeug.utils.secure_filename(imagefile.filename)
-       print("\nReceived image File name : " + imagefile.filename)
-       imagefile.save(filename)
-       return "Image Uploaded Successfully"
-       #se questo codice va bene, poi bisogna inglobare quello di Matteo e fare il return della nuova immagine direttamente
 
 @app.route('/connection_test')
 def prova():
